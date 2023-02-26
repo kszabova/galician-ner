31f03bdcb51817ee395a58d19020c2b4994a93cb
Kristina Szabova <k.szabova98@gmail.com>
2023-02-26 17:17:32 +0100

    Implement demo for transformers

--- ner_transformers.py
+++ ner_transformers.py
@@ -15,17 +15,9 @@ from datasets import Dataset
 from xml_webanno_parser import get_entities_for_transformers
 from util import get_files_in_directory, split_train_test
 
-nlp = spacy.blank("en")
-files = get_files_in_directory("data")
-data = get_entities_for_transformers(files, ["BODYPART", "CLINENTITY"], nlp)
-
-train_data, test_data = split_train_test(data, random_seed=42)
-
-df_train = pd.DataFrame(train_data, columns=["text", "tags"])
-df_test = pd.DataFrame(test_data, columns=["text", "tags"])
-
 
 def compute_metrics(eval_pred):
+    # TODO this is not working
     logits, labels = eval_pred
     predictions = np.argmax(logits, axis=-1)
     accuracy = evaluate.load("accuracy")
@@ -106,7 +98,7 @@ def prepare_trainer(train, test, tags):
         args=training_args,
         train_dataset=train,
         eval_dataset=test,
-        ompute_metrics=compute_metrics,
+        # compute_metrics=compute_metrics,
     )
 
     return trainer
@@ -127,12 +119,35 @@ def run_model(train, demo, data_dir, tags):
                 "labels_to_ids": labels_to_ids,
                 "ids_to_labels": ids_to_labels,
             }
-            f.write(json.dumps(label_mapping), indent=4)
+            f.write(json.dumps(label_mapping, indent=4))
         train_dataset = dataframe_to_dataset(df_train, tokenizer, labels_to_ids)
         test_dataset = dataframe_to_dataset(df_test, tokenizer, labels_to_ids)
         trainer = prepare_trainer(train_dataset, test_dataset, tags)
         train_model(trainer)
     if demo:
-        # TODO: implement demo
-        pass
+        saved_model = BertForTokenClassification.from_pretrained("transformer_model")
+        nlp = spacy.blank("en")
+        with open("labels.json", "r") as f:
+            label_mapping = json.load(f)
+            ids_to_labels = label_mapping["ids_to_labels"]
+        while True:
+            text = input("Enter text (or 'exit' to quit):\n")
+            tokens = [tok.text for tok in nlp.tokenizer(text)]
+            tokenized = tokenizer(
+                tokens,
+                is_split_into_words=True,
+                padding="max_length",
+                truncation=True,
+                return_tensors="pt",
+            )
+            predicted_label_ids = saved_model(**tokenized).logits.argmax(-1)
+            labels = []
+            previous_word_id = None
+            for word_id, label in zip(tokenized.word_ids(), predicted_label_ids[0]):
+                if word_id is None or word_id == previous_word_id:
+                    continue
+                else:
+                    labels.append(ids_to_labels[label.item()])
+                    previous_word_id = word_id
+            print(*zip(tokens, labels), sep="\n")
 
